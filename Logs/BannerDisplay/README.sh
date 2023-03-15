d56b3b427eb70cf85057508ffaf9bb7409232053
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2023-03-15 15:08:53 -0700

    README.sh
    README.sh

---
+++ Logs/BannerDisplay/README.sh
@@ -0,0 +1,28 @@
+pi@raspberrypi:~ $ neofetch
+  `.::///+:/-.        --///+//-:``    pi@raspberrypi 
+ `+oooooooooooo:   `+oooooooooooo:    -------------- 
+  /oooo++//ooooo:  ooooo+//+ooooo.    OS: Raspbian GNU/Linux 10 (buster) armv7l 
+  `+ooooooo:-:oo-  +o+::/ooooooo:     Host: Raspberry Pi 4 Model B Rev 1.4 
+   `:oooooooo+``    `.oooooooo+-      Kernel: 5.4.51-v7l+ 
+     `:++ooo/.        :+ooo+/.`       Uptime: 4 hours, 6 mins 
+        ...`  `.----.` ``..           Packages: 2002 (dpkg) 
+     .::::-``:::::::::.`-:::-`        Shell: bash 5.0.3 
+    -:::-`   .:::::::-`  `-:::-       Resolution: 1920x1080, 1920x1080 
+   `::.  `.--.`  `` `.---.``.::`      DE: LXDE 
+       .::::::::`  -::::::::` `       WM: Openbox 
+ .::` .:::::::::- `::::::::::``::.    Theme: Adwaita [GTK3] 
+-:::` ::::::::::.  ::::::::::.`:::-   Icons: Adwaita [GTK3] 
+::::  -::::::::.   `-::::::::  ::::   Terminal: lxterminal 
+-::-   .-:::-.``....``.-::-.   -::-   Terminal Font: Monospace 15 
+ .. ``       .::::::::.     `..`..    CPU: BCM2711 (4) @ 1.500GHz 
+   -:::-`   -::::::::::`  .:::::`     Memory: 210MiB / 7875MiB 
+   :::::::` -::::::::::` :::::::.
+   .:::::::  -::::::::. ::::::::                              
+    `-:::::`   ..--.`   ::::::.
+      `...`  `...--..`  `...`
+            .::::::::::
+             `.-::::-`
+
+pi@raspberrypi:~ $ # This is my first Raspberry Pi
+pi@raspberrypi:~ $ 
+
